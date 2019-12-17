a33dd7e84bf77315215a94666dbb1a7faec5cf66
Changhoon Ji <cji2@gmu.edu>
2019-12-16 23:46:02 -0500

    reading HTML form with HTML form & doGET() Servlet file

---
+++ servlet-intro/src/edu/gmu/servletdemo/StudentServlet.java
@@ -0,0 +1,47 @@
+package edu.gmu.servletdemo;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class StudentServlet
+ */
+@WebServlet("/StudentServlet")
+public class StudentServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public StudentServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// setting content type.
+		response.setContentType("text/html");
+		
+		// getting print writer.
+		PrintWriter out = response.getWriter();
+		
+		// generate HTML content
+		out.println("<html><body>");
+		out.println("<h4>The student is confirmed: " 
+				+ request.getParameter("firstName") + " " 
+				+ request.getParameter("lastName") + "</h4>");
+		out.println("</body></html>");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
